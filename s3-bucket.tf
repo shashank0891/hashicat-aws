436b3c51d2cd159d2f8d80e30773d21127f13822
Shashank Sharma <shashanksharma0891@gmail.com>
2021-05-26 19:22:18 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,6 @@
-module "aws_s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/shsharma-training/s3-bucket/aws"
+  version = "2.2.0"
+  # insert required variables here
+  bucket_prefix = "my_s3_bucket"
 }
